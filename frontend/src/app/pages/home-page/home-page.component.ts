5220b7741cd1b2898b06b1d501b85c24918644b7
Mihajlo <mihajlo0903@gmail.com>
2026-02-17 12:29:13 +0100

    added cards on front for contorlling rgb and 7-segment display

--- frontend/src/app/pages/home-page/home-page.component.ts
+++ frontend/src/app/pages/home-page/home-page.component.ts
@@ -2,15 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { AlarmState } from '../../models/alarm/alarm-state';
 import { CommonModule } from '@angular/common';
 import { AlarmService } from '../../services/alarm.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent implements OnInit {
+
+  displayValue: number = 0;
+  selectedColor: string = '#ff0000';
+  secondsIncrement: number = 5;
 
   states = Object.values(AlarmState);
   currentState: AlarmState | null = null;
@@ -37,5 +45,17 @@ export class HomePageComponent implements OnInit {
 
   isDisabled(state: AlarmState) {
     return state === AlarmState.ARMING;
+  }
+
+  setDisplay() {
+    console.log("Set display:", this.displayValue);
+  }
+
+  setSecondsIncrement() {
+    console.log("Set seconds increment:", this.secondsIncrement);
+  }
+
+  setColor() {
+    console.log("Set color:", this.selectedColor);
   }
 }
